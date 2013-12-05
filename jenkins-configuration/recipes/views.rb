ee7c430227593670c5ff000fa53a092b85842bf7
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-12-05 09:12:55 -1000

    call the cli to execute the view script.

--- jenkins-configuration/recipes/views.rb
+++ jenkins-configuration/recipes/views.rb
@@ -1,4 +1,12 @@
+jenkins_url = "http://localhost:8080"
+# jenkins_home = '/var/lib/jenkins'
+
 cookbook_file "script to add Jenkins views" do
   source "create_views.groovy"
   path "/tmp/create_views.groovy"
+end
+
+jenkins_cli "add infrastructure view" do
+  url jenkins_url
+  command "groovy /tmp/create_views.groovy"
 end
