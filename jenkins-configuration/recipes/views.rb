efa1e006f462a3709254979533a5e575a764eb8d
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-12-06 09:31:39 -1000

    restarting and sleeping to see if that gets us around the stupid classloader issue.

--- jenkins-configuration/recipes/views.rb
+++ jenkins-configuration/recipes/views.rb
@@ -1,6 +1,18 @@
 jenkins_url = "http://localhost:8080"
 # jenkins_home = '/var/lib/jenkins'
 
+#classloader problem with asking groovy plugin to do stuff
+service "jenkins" do
+  action :restart
+end
+
+ruby_block "sleep a bit" do
+  block do
+    sleep 60
+  end
+  action :create
+end
+
 cookbook_file "script to add Jenkins views" do
   source "create_views.groovy"
   path "/tmp/create_views.groovy"
