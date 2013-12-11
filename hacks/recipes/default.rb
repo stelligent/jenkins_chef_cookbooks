e48d77275e8b3883ea3f89b8382afe55a4345cda
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-12-11 11:17:49 -1000

    lets try moving the gem exe where it's looking for it.

--- hacks/recipes/default.rb
+++ hacks/recipes/default.rb
@@ -3,3 +3,9 @@ bash "install ruby gems bundler" do
   code "gem install rubygems-bundler && echo complete"
   user "jenkins"
 end
+
+bash "soft link gem exe" do
+  code "ln -s /usr/bin/gem /usr/local/bin/gem"
+  user "jenkins"
+end
+
