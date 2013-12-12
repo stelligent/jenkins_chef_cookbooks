39fe22d36868375f9202b67007b8c6136ffeee0a
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-12-11 16:25:00 -1000

    adding debugging output

--- jenkins-configuration/recipes/config.rb
+++ jenkins-configuration/recipes/config.rb
@@ -1,4 +1,6 @@
-# bash "install ruby because rvm cookbook freaks out" do
-#   code "[[ -s '/var/lib/jenkins/.rvm/scripts/rvm' ]] && source '/var/lib/jenkins/.rvm/scripts/rvm' && rvm install #{node[:jenkins][:ruby_version]} --verify-downloads 1"
-#   user "jenkins"
-# end
+bash "install ruby because rvm cookbook freaks out" do
+  Chef::Log.debug("installing ruby")
+  code "[[ -s '/var/lib/jenkins/.rvm/scripts/rvm' ]] && source '/var/lib/jenkins/.rvm/scripts/rvm' && rvm install 1.9.3 --verify-downloads 1"
+  user "jenkins"
+  Chef::Log.debug("ruby installed")
+end
