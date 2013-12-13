ee80965e8f3cb3bc4fabdf1f54156948962ea063
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-12-13 13:06:56 -1000

    okay, that firefox cookbook was windows only, so let's try this.

--- firefox/recipes/default.rb
+++ firefox/recipes/default.rb
@@ -1,27 +1,12 @@
-# Cookbook Name:: firefox
-# Recipe:: default
-#
-# Copyright 2012, Webtrends, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+bash "install_firefox" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+    . /tmp/gtk-firefox.sh
+  EOH
+end
 
-if platform?('windows')
-  windows_package 'Mozilla Firefox 25.0.1 (x86 en-US)' do
-    source node['firefox']['http_url']
-    installer_type :custom
-    options '-ms'
-    action :install
-  end
-else
-  Chef::Log.warn('Firefox can only be installed on the Windows platform using this cookbook.')
+cookbook_file "script to install Firefox" do
+  source "gtk-firefox.sh"
+  path "/tmp/gtk-firefox.sh"
 end
