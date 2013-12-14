1b67f94984bd89a3f74550c92a8ab1cb74331a30
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-12-13 15:00:52 -1000

    order is important.

--- firefox/recipes/default.rb
+++ firefox/recipes/default.rb
@@ -1,3 +1,8 @@
+cookbook_file "script to install Firefox" do
+  source "gtk-firefox.sh"
+  path "/tmp/gtk-firefox.sh"
+end
+ 
 bash "install_firefox" do
   user "root"
   cwd "/tmp"
@@ -6,7 +11,3 @@ bash "install_firefox" do
   EOH
 end
 
-cookbook_file "script to install Firefox" do
-  source "gtk-firefox.sh"
-  path "/tmp/gtk-firefox.sh"
-end
