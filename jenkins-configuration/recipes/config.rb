cab802cd4704c5bead41189377551f51dc16d67b
Eric Kascic <eric.kascic@stelligent.com>
2014-01-09 11:41:12 -0500

    attempt to uninstall 1.9.3 first so rvm doesnt mistakenly think its already extracted also guard against reinstalling if its already there

--- jenkins-configuration/recipes/config.rb
+++ jenkins-configuration/recipes/config.rb
@@ -1,6 +1,7 @@
 bash "install ruby because rvm cookbook freaks out" do
-  Chef::Log.debug("installing ruby")
-  code "[[ -s '/var/lib/jenkins/.rvm/scripts/rvm' ]] && source '/var/lib/jenkins/.rvm/scripts/rvm' && rvm install 1.9.3 --verify-downloads 1"
+  #for some reason, rvm thinks 1.9.3 is already extracted but its not so configure or make will fail.  makes no sense
+  #uninstall 1.9.3 in attempt to cleanse anything that could be misconstrued
+  code "/var/lib/jenkins/.rvm/bin/rvm uninstall 1.9.3 && /var/lib/jenkins/.rvm/bin/rvm install 1.9.3 --verify-downloads 1"
   user "jenkins"
-  Chef::Log.debug("ruby installed")
+  not_if '/var/lib/jenkins/.rvm/bin/rvm list strings | grep ruby-1.9.3', :user => 'jenkins'
 end
